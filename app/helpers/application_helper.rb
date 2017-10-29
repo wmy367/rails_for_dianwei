dde41719d40a4a770b7771a91931e29ff5ad9e24
--@--Young--@-- <wmy367@gmail.com>
2017-10-29 17:04:50 +0800

    m title ; notice show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
     def full_title(page_title)
-        # base_title = "湖南大学 电子义务维修基地"
-        base_title = "例程"
+        base_title = "湖南大学 电子义务维修基地"
+        # base_title = "例程"
 
         if page_title.empty?
             base_title
