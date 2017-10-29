f6bf3617079cd389dbd4ea0d6a9cebc87da50c4e
--@--Young--@-- <wmy367@gmail.com>
2017-10-29 15:54:51 +0800

    fix school id

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
     validates_presence_of :password_confirmation,message: "密码验证不能为空",if: :password_confirmation
 
 
-    VALID_SCH_REGEX = /20[0-9]{2}1120[1-4][0-9]{2}/
+    VALID_SCH_REGEX = /20[0-9]{2}110?20[1-4][0-9]{2}/
     validates :school_serial,presence: true ,length: {in: 11..12 },uniqueness: true,format:{with:VALID_SCH_REGEX}
     validates :phone,length:{maximum: 24}
 
